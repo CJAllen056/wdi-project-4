e793c4969a19839eefafcda46707768b13b84f83
CJAllen056 <christopherjallen056@gmail.com>
2016-05-29 21:28:07 +0100

    Pretty much copied across rest of app.js from walkthrough

--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,36 @@
 angular
 .module("sketchApp", ["ngResource", "angular-jwt", "ui.router"])
 .constant("API", ["http://localhost:3000/api"]);
+.config(mainRouter);
+
+mainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
+function mainRouter($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "./js/views/home.html"
+    })
+    .state('login', {
+      url: "/login",
+      templateUrl: "./js/views/authentications/login.html"
+    })
+    .state('register', {
+      url: "/register",
+      templateUrl: "./js/views/authentications/register.html"
+    })
+    .state('users', {
+      url: "/users",
+      templateUrl: "./js/views/users/index.html"
+    })
+    .state('user', {
+      url: "/users/:id",
+      templateUrl: "./js/views/users/show.html",
+      controller: function($scope, $stateParams, User) {
+        User.get({ id: $stateParams.id }, function(res){
+          $scope.$parent.users.user = res.user;
+        });
+      }
+    });
+
+  $urlRouterProvider.otherwise("/");
+}
