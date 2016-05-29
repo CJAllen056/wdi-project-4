1189982ad6a25ee8fd78c4f81d474575cc8106b2
CJAllen056 <christopherjallen056@gmail.com>
2016-05-29 15:25:14 +0100

    FUCK, forgot to save. last commit probably didn't have the stuff in app.js, but it should be there now

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 var config  = require("./config/config");
 var express = require("express");
 var app     = express();
+
+app.listen(config.port, function() {
+  console.log("Sketch-off is running on port ", port);
+});
