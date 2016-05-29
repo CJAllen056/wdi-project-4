db845c610eed26f0f618e901e686d6164081148d
CJAllen056 <christopherjallen056@gmail.com>
2016-05-29 15:30:38 +0100

    installed morgan and method override and added middleware

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-var config  = require("./config/config");
-var express = require("express");
+var config          = require("./config/config");
+var express         = require("express");
+var morgan          = require("morgan");
+var methodOverride  = require("method-override");
+
 var app     = express();
+
+app.use(morgan("dev"));
+
+app.use(methodOverride(function(req, res) {
+  if (req.body && typeof req.body === "object" && "_method" in req.body) {
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
 
 app.listen(config.port, function() {
   console.log("Sketch-off is running on port ", port);
