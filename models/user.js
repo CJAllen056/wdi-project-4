d8c833643667f448851dc807be1208cd57832c0d
CJAllen056 <christopherjallen056@gmail.com>
2016-05-29 16:13:18 +0100

    added bcrypt and validator and set up basic validations on user model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
-var mongoose = require("mongoose");
+var mongoose  = require("mongoose");
+var bcrypt    = require("bcrypt-nodejs");
+var validator = require("validator");
 
 var userSchema = new mongoose.Schema({
   fName:        { type: String },
@@ -9,6 +11,50 @@ var userSchema = new mongoose.Schema({
   passwordHash: { type: String }
 }, {
   timestamps:   true
+});
+
+userSchema.methods.validatePassword = function() {
+  return bcrypt.compareSync(password, this.passwordHash, null);
+};
+
+userSchema.virtual("password")
+.set(function(password) {
+  this._password    = password;
+  this.passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+});
+
+userSchema.virtual("passwordConfirmation")
+.set(function(passwordConfirmation) {
+  this._passwordConfirmation = passwordConfirmation;
+});
+
+userSchema.path("passwordHash")
+.validate(function() {
+  if (this.isNew) {
+    if (!this._password) {
+      this.invalidate("password", "A password is required");
+    }
+    if (this._password.length < 8) {
+      this.invalidate("password", "Passwords must be 8 characters or more");
+    }
+    if (this._password !== this._passwordConfirmation) {
+      this.invalidate("passwordConfirmation", "The password confirmation and password do not match");
+    }
+  }
+});
+
+userSchema.path("email")
+.validate(function(email) {
+  if (!validator.isEmail(email)) {
+    this.invalidate("email", "This is not a valid email");
+  }
+});
+
+userSchema.set("toJSON", {
+  tranform: function(doc, ret, options) {
+    delete ret.passwordHash;
+    return ret;
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
