821418d08ac5ec044a05e7ba840ab5280596769d
CJAllen056 <christopherjallen056@gmail.com>
2016-05-29 21:32:53 +0100

    Pretty much copied views from walkthrough

--- public/js/client.js
+++ public/js/client.js
@@ -1,6 +1,6 @@
 angular
 .module("sketchApp", ["ngResource", "angular-jwt", "ui.router"])
-.constant("API", ["http://localhost:3000/api"]);
+.constant("API", ["http://localhost:3000/api"])
 .config(mainRouter);
 
 mainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
