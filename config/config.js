49328b99e0eb1b5c5cf6e592aa1e4e9e2fbcc0c0
CJAllen056 <christopherjallen056@gmail.com>
2016-05-30 14:44:48 +0100

    changed database to be for this app

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port:     process.env.PORT || 3000,
-  database: process.env.MONGOLAB_URI || "mongodb://localhost/passport-jwt-boilerplate",
+  database: process.env.MONGOLAB_URI || "mongodb://localhost/sketch-app",
   secret:   process.env.SECRET || "aB(^eEAdaa&&#ak@ahjb82#sa#£gpke2"
 };
