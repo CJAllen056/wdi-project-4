bf5c66bd638e399904ff15d4f1f1ec03ae2c0d82
CJAllen056 <christopherjallen056@gmail.com>
2016-05-31 11:02:05 +0100

    pretty much copied js for canvas drawing from stack overflow, but it doesn't work

---
+++ src/js/canvas.js
@@ -0,0 +1,55 @@
+angular
+.module("sketchApp")
+.directive("canvasDraw", drawOnCanvas);
+
+function drawOnCanvas() {
+  console.log("hello");
+  return {
+    restrict: "A",
+    link: function(scope, element) {
+      var ctx     = element[0].getContext("2d");
+      var drawing = false;
+      var lastX;
+      var lastY;
+
+      element.bind("mousedown", function(event) {
+        if (event.offsetX !== undefined) {
+          lastX = event.offsetX;
+          lastY = event.offsetY;
+        }
+
+        ctx.beginPath();
+        drawing = true;
+      });
+
+      element.bind("mousemove", function(event) {
+        if (drawing) {
+          if (event.offsetX !== undefined) {
+            currentX = event.offsetX;
+            currentY = event.offsetY;
+          }
+
+          draw(lastX, lastY, currentX, currentY);
+
+          lastX = currentX;
+          lastY = currentY;
+        }
+      });
+
+      element.bind("mouseup", function(event) {
+        drawing = false;
+      });
+
+      function reset() {
+        element[0].width = element[0].width;
+      }
+
+      function draw(lX, lY, cX, cY) {
+        ctx.moveTo(lX,lY);
+        ctx.lineTo(cX,cY);
+        ctx.strokeStyle = "black";
+        ctx.stroke();
+      }
+    }
+  };
+}
