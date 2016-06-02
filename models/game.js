c764f5a80136874ab70094d9638ef0660583540d
CJAllen056 <christopherjallen056@gmail.com>
2016-06-02 10:38:57 +0100

    Wrote some seed games

--- models/game.js
+++ models/game.js
@@ -1,9 +1,11 @@
 var mongoose  = require("mongoose");
 
 var gameSchema = new mongoose.Schema({
-  topic:    { type: String },
-  prompts:  { type: Array },
-  users:    [{ type: mongoose.Schema.ObjectId, ref: "User" }]
+  topic:        { type: String },
+  prompts:      { type: Array },
+  users:        [{ type: mongoose.Schema.ObjectId, ref: "User" }],
+  roundLength:  { type: Number },
+  difficulty:   { type: String }
 });
 
 module.exports = mongoose.model("Game", gameSchema);
