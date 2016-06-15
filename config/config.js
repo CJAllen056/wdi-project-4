d4ce302d41ec9cdce50ba4dad5e6a19f0e334a60
CJAllen056 <christopherjallen056@gmail.com>
2016-06-15 15:06:42 +0100

    changed config database to heroku database

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port:     process.env.PORT || 3000,
-  database: process.env.MONGOLAB_URI || "mongodb://localhost/sketch-app",
+  database: process.env.MONGODB_URI || "mongodb://localhost/sketch-app",
   secret:   process.env.SECRET || "aB(^eEAdaa&&#ak@ahjb82#sa#£gpke2"
 };
